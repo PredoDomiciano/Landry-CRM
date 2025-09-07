787073b741a766076dbe12d50111891af64b7395
Pedro Domiciano <pedrohenrique4254@gmail.com>
2025-09-07 10:24:02 -0300

    Adiciona projeto backend

---
+++ backend/landryJoias/src/test/java/com/Landry/landryJoias/LandryJoiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Landry.landryJoias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandryJoiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
